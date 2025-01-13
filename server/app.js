136121d1c3217bdf25931bc8e334607c4e41b526
mjmares <142510831+mjmares@users.noreply.github.com>
2025-01-13 14:13:39 -0600

    Server

---
+++ server/app.js
@@ -0,0 +1,49 @@
+// Import required modules
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const db = require("./config/database");
+
+// Initialize express app
+const app = express();
+
+// Database connection setup
+// db.connect(err => {
+//     if (err) {
+//         console.error("Database connection error:", err);
+//     } else {
+//         console.log("Successfully connected to the database.");
+//     }
+// });
+
+// Middleware setup
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(express.json()); // Parse incoming JSON requests
+app.use(cookieParser()); // Parse cookies
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Static file serving for public assets (JS, CSS, Images)
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+app.use("/img", express.static("./public/img"));
+
+// Route setup for page routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
+// Add in 404 page
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: '404 Not Found',
+        message: 'The resource you are looking for does not exist.',
+    });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}`);
+});
---
+++ server/routes/pages.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+// Home Page
+router.get("/", (req, res) => {
+    return res.status(200).render("index");
+})
+
+// About Page
+router.get("/about", (req, res) => {
+    return res.status(200).render("about");
+})
+
+// Help Page
+router.get("/help", (req, res) => {
+    return res.status(200).render("help");
+})
+
+// Dashboard Page
+router.get("/dash", (req, res) => {
+    return res.status(200).render("dash");
+})
+
+// User Account Page
+router.get("/account", (req, res) => {
+    return res.status(200).render("account");
+})
+
+module.exports = router;
