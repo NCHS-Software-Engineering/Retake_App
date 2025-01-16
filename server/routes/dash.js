141efb2188e1f7ed6289fc8e296469f6c248172f
mjmares <142510831+mjmares@users.noreply.github.com>
2025-01-16 14:19:32 -0600

    misc

--- server/routes/dash.js
+++ server/routes/dash.js
@@ -7,5 +7,28 @@ const teacherDashController = require("../controllers/teacherDashController");
 // Student Dashboard Controllers
 const studentDashController = require("../controllers/studentDashController");
 
+// --------------------------------------------------
+// Teacher Routes
+// --------------------------------------------------
+
+// Manage Classes
+router.get("/classes", (req, res) => {
+    return res.status(200).render("/dash/teacher/manageClasses");
+})
+
+// Manage Email
+router.get("/email", (req, res) => {
+    return res.status(200).render("/dash/teacher/email");
+})
+
+// --------------------------------------------------
+// Student Routes
+// --------------------------------------------------
+
+// Track retakes (past/current)
+router.get("/tracker", (req, res) => {
+    return res.status(200).render("/dash/student/tracker");
+})
+
 
 module.exports = router;
