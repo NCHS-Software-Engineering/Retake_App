b9a4c0f8fb2ad72b68b5bc1540a00e46045bd094
AustinJamesZiliak <Ajziliak@stu.naperville203.org>
2025-03-02 20:44:38 -0600

    added google log in

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,51 @@
 const pool = require("../config/database");
 const config = require("../config/config");
 const { createToken } = require("../middleware/jwt");
+
+
+
+exports.login = async (req, res) => {
+    const { email, username, googleToken } = req.body;
+
+
+    if (email.endsWith('@stu.naperville203.org') || email.endsWith('@naperville203.org')) {
+    try {
+        const role = "";
+        if (email.endsWith('@stu.naperville203.org')) {
+            role = "student";
+        } else if (email.endsWith('@naperville203.org')) {
+            role = "teacher";
+        }
+        const [rows] = await pool.query('SELECT * FROM users WHERE googleToken = ?', [googleToken]);
+
+        if (rows.length === 0) {
+            const [result] = await pool.query(
+                'INSERT INTO users (username, googleToken, type) VALUES (?, ?, ?)', [
+                    username,
+                    googleToken,
+                    role,
+                ]
+            );
+            
+        }
+
+        const user = rows[0];
+
+        const tokenData = { token: googleToken, id: user.userId, type: user.type };
+        const accessToken = createToken(tokenData);
+
+        res.cookie('access-token', accessToken, {
+            maxAge: 2592000000,
+            httpOnly: true
+        })
+
+        return res.status(200).redirect("/dash");
+    } catch (err) {
+        console.error('Error during login:', err);
+        return res.status(500).render("pages/auth", { err: 'An error occurred, please try again later.' });
+    }
+    }
+    else {
+        return res.status(500).render("pages/auth", { err: 'Invalid email adress.' });
+    }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,4 +4,6 @@ const router = express();
 // Controllers
 const authController = require("../controllers/authController");
 
+router.post("/login", authController.login)
+
 module.exports = router;
