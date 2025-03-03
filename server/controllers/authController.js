0a6b21966ac620e9a255881abc2d0daee1f985ba
AustinJamesZiliak <142603126+AustinJamesZiliak@users.noreply.github.com>
2025-03-03 14:13:57 -0600

    fixed log in

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,41 +9,43 @@ exports.login = async (req, res) => {
 
 
     if (email.endsWith('@stu.naperville203.org') || email.endsWith('@naperville203.org')) {
-    try {
-        const role = "";
-        if (email.endsWith('@stu.naperville203.org')) {
-            role = "student";
-        } else if (email.endsWith('@naperville203.org')) {
-            role = "teacher";
-        }
-        const [rows] = await pool.query('SELECT * FROM users WHERE googleToken = ?', [googleToken]);
+        try {
+            const role = "";
+            if (email.endsWith('@stu.naperville203.org')) {
+                role = "student";
+            } else if (email.endsWith('@naperville203.org')) {
+                role = "teacher";
+            }
+            const [rows] = await pool.query('SELECT * FROM users WHERE googleToken = ?', [googleToken]);
 
-        if (rows.length === 0) {
-            const [result] = await pool.query(
-                'INSERT INTO users (username, googleToken, type) VALUES (?, ?, ?)', [
+            if (rows.length === 0) {
+                const [result] = await pool.query(
+                    'INSERT INTO users (username, googleToken, type) VALUES (?, ?, ?)', [
                     username,
                     googleToken,
                     role,
                 ]
-            );
-            
+                );
+
+                rows[0] = { userId: result.insertId, type: role };
+            }
+
+
+            const user = rows[0];
+
+            const tokenData = { token: googleToken, id: user.userId, type: user.type };
+            const accessToken = createToken(tokenData);
+
+            res.cookie('access-token', accessToken, {
+                maxAge: 2592000000,
+                httpOnly: true
+            })
+
+            return res.status(200).redirect("/dash");
+        } catch (err) {
+            console.error('Error during login:', err);
+            return res.status(500).render("pages/auth", { err: 'An error occurred, please try again later.' });
         }
-
-        const user = rows[0];
-
-        const tokenData = { token: googleToken, id: user.userId, type: user.type };
-        const accessToken = createToken(tokenData);
-
-        res.cookie('access-token', accessToken, {
-            maxAge: 2592000000,
-            httpOnly: true
-        })
-
-        return res.status(200).redirect("/dash");
-    } catch (err) {
-        console.error('Error during login:', err);
-        return res.status(500).render("pages/auth", { err: 'An error occurred, please try again later.' });
-    }
     }
     else {
         return res.status(500).render("pages/auth", { err: 'Invalid email adress.' });
